df99148e4193598fc4dc0c9f8bbb87e0cd7fbfc8
Mazhar Altincay <mazharaltincay89@gmail.com>
2024-10-05 01:05:28 +0200

    java remember

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,20 @@
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
         //ppj28
         Album alb1 = new Album("Loving You",
                 new Song[]{
-                        new Song("Elvis","Party"),
-                        new Song("Elvis","Hot Dog")
+                        new Song("Elvis", "Party"),
+                        new Song("Elvis", "Hot Dog")
                 });
         Album alb2 = new Album("Ella & Louis",
                 new Song[]{
-                        new Song("Armstrong","A Foggy Day"),
-                        new Song("Fitzgerald","Cheek to Cheek")
+                        new Song("Armstrong", "A Foggy Day"),
+                        new Song("Fitzgerald", "Cheek to Cheek")
                 });
         Album alb3 = new Album("The Best Of",
                 new Song[]{
-                        new Song("Elvis","I Love You Because"),
-                        new Song("Sinatra","My Way")
+                        new Song("Elvis", "I Love You Because"),
+                        new Song("Sinatra", "My Way")
                 });
 
         Shelf shelf = new Shelf(new Album[]{alb1, alb2, alb3});
@@ -26,43 +24,43 @@ public class Main {
 
         Song[] armstrongSongs = shelf.findSongsBySinger("Armstrong");
 
-        for(int i = 0; i < elvisSongs.length; i++)
+        for (int i = 0; i < elvisSongs.length; i++)
             System.out.println(elvisSongs[i]);
 
-        for(int i = 0; i < armstrongSongs.length; i++)
+        for (int i = 0; i < armstrongSongs.length; i++)
             System.out.println(armstrongSongs[i]);
 
         //ppj25
-            //1
-                /*try{
+        //1
+        try {
             MyClass.myMethod();
-        }catch (MyException e){
+        } catch (MyException e) {
             e.printStackTrace();
         }
 
-        try{
+        try {
             MyClass.myMethod2();
-        }catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
-        }*/
-                try{
-                    SmokeDetector.check();
-                }catch(Alarm alarm){
-                    System.out.println(alarm.getMessage());
-                }
+        }
+        try {
+            SmokeDetector.check();
+        } catch (Alarm alarm) {
+            System.out.println(alarm.getMessage());
+        }
         //2
 
-            //3
-                try{
-                    Rocket r = new Rocket("RADI", 400);
-                    r.refuel();
-                    r.refuel();
-                    r.refuel();
-                    r.refuel();
-                    r.start();
-                }catch (Exception e){
-                    e.printStackTrace();
-                }
+        //3
+        try {
+            Rocket r = new Rocket("RADI", 400);
+            r.refuel();
+            r.refuel();
+            r.refuel();
+            r.refuel();
+            r.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
